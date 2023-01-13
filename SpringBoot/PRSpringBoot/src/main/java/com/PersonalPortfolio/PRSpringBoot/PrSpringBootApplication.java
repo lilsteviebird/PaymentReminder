b1f92b42b4e0fa8bcb29d64ed1cb0f6d02da33a0
lilsteviebird <stevenli880@gmail.com>
2023-01-13 15:32:33 -0500

    initial commit

---
+++ SpringBoot/PRSpringBoot/src/main/java/com/PersonalPortfolio/PRSpringBoot/PrSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.PersonalPortfolio.PRSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrSpringBootApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/PRSpringBoot/src/test/java/com/PersonalPortfolio/PRSpringBoot/PrSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PersonalPortfolio.PRSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
